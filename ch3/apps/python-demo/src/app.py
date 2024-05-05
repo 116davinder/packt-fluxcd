02ca8ba26c9df794dbef5921910c4eb665c1bde4
Davinder Pal <dpsangwal@gmail.com>
2024-05-05 21:49:57 +0300

    tag image two times

--- ch3/apps/python-demo/src/app.py
+++ ch3/apps/python-demo/src/app.py
@@ -1,11 +1,12 @@
-from flask import Flask 
+from flask import Flask
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
-@app.route('/') 
+@app.route('/')
 
-def hello_packt(): 
-    return 'Hello Packt' 
+def hello_packt():
+    """Return Hello Packt if called"""
+    return 'Hello Packt'
 
-if __name__ == '__main__': 
-    app.run(host="0.0.0.0", port=5000) 
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
